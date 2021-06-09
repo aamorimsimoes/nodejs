5829071d6f99a5467e697941de2df52b2aa69f19
aamorimsimoes <andreamorimsimoes@gmail.com>
2021-06-09 19:19:56 +0100

    hide mongodb connection

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const ejs = require('ejs');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const Blog = require('./models/blog');
+const secret = require('./secret');
 
 // express app
 const app = express();
 
 // connect to mongodb
-const dbURI = 'mongodb+srv://amorim:8E7TBPYTWKVpK9k1@nodejs.wullc.mongodb.net/express?retryWrites=true&w=majority';
+const dbURI = secret.secret;
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => app.listen(3000), console.log('connected to db'))
   .catch((err) => console.log(err));
@@ -20,74 +21,29 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
-// mongoose and mongo sandbox routes
-app.get('/add-blog', (req, res) => {
-  const blog = new Blog({
-    title: ' new blog 2', 
-    snippet: 'about my blog', 
-    body: 'more info about my new blog'
-  });
-
-  blog.save()
-    .then((result) => {
-      res.send(result)
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-})
-
-app.get('/all-blogs', (req, res) => {
-  Blog.find()
-  .then((result) => {
-    res.send(result)
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-})
-
-app.get('/single-blog', (req, res) => {
-  Blog.findById('60bff822dd8f29360499a962')
-  .then((result) => {
-    res.send(result)
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-})
-
-// middleware
-// app.use((req, res, next) => {
-//   console.log('new request made:');
-//   console.log('host:', req.hostname);
-//   console.log('path:', req.path);
-//   console.log('method:', req.method);
-//   next();
-// });
-
-// app.use((req, res, next) => {
-//   console.log('in the next middleware');
-//   next();
-// });
 
 
-
+// routes
 app.get('/', (req, res) => {
-  const blogs = [
-    {title: 'Parturient aptent est orci rutrum.', snippet: 'Nibh risus. Parturient aptent est orci rutrum, eros mattis nec elit metus faucibus habitant iaculis tincidunt tempus montes amet conubia quisque, porttitor. Duis libero ullamcorper per hendrerit purus aptent tellus auctor inceptos.'},
-    {title: 'Integer vel tempus, hac litora cras.', snippet: 'Integer vel tempus, hac litora cras. Imperdiet ligula, class, luctus tempus purus id ipsum massa est feugiat curabitur. Curae; eros gravida nam. Diam curabitur dignissim taciti.'},
-    {title: 'Rutrum natoque nisl.', snippet: 'Rutrum natoque nisl. Nec posuere. Mattis odio pellentesque Nibh semper aenean montes sed vel litora senectus tempor aliquam nulla habitant vestibulum, tempus, a nullam curae;. Varius non, velit diam nascetur, torquent facilisis urna, nostra quisque non dolor varius quis dui Lacinia quis.'},
-  ];
-
-  res.render('index', { title: 'expressJS', blogs});
+  res.redirect('/blogs');
 })
 
 app.get('/about', (req, res) => {
   const aboutus = [
-    {body: 'Consequat sit torquent enim suscipit mus ipsum, lectus justo sociis vulputate pulvinar nulla turpis ut nec, dis odio interdum elit. Lorem lacus natoque mauris erat enim rutrum egestas rutrum mauris nam natoque suspendisse quisque purus. Ipsum dolor vivamus, imperdiet arcu sociosqu viverra vel interdum. Dignissim cum potenti mi. Consequat habitant viverra at vitae malesuada interdum. Turpis quam laoreet est conubia in hendrerit ultricies velit facilisi malesuada metus dignissim duis sollicitudin fusce velit dis posuere ullamcorper a duis id, feugiat, curae; iaculis donec, cras tempor diam dapibus nascetur. Fames mattis adipiscing bibendum magna sodales rutrum lobortis parturient sociis purus. Viverra ipsum ornare, taciti in. Mauris libero, facilisi hymenaeos Dis. Nibh nullam curabitur cubilia orci Sagittis diam sit mauris egestas Ac hymenaeos auctor purus. Sollicitudin. At bibendum vel justo parturient etiam tempus torquent ornare rutrum. Parturient nunc posuere nulla eros varius sodales eleifend conubia class molestie ac etiam. Mauris cursus potenti consectetuer a nullam etiam bibendum metus id convallis orci quis malesuada commodo. Convallis. Lacus. Cras. Lacinia urna semper odio eleifend aenean. Mauris viverra ante vel sem malesuada cubilia lacus, placerat facilisis gravida aliquam et hymenaeos tellus penatibus libero quisque sociosqu ridiculus donec eleifend commodo Imperdiet praesent rhoncus vehicula diam fames curae;. Malesuada. Duis posuere ac. Sociis suspendisse magnis nonummy. Nam litora aptent blandit nisi. Pulvinar diam. Blandit amet porttitor nam non urna lacinia eget Sed molestie mauris aliquam ridiculus arcu congue conubia malesuada mattis. Quis nascetur sodales primis laoreet lectus fames suscipit netus. Lacus vehicula dictumst mattis nam non platea orci duis senectus placerat est tortor vestibulum porta sed tortor risus pharetra libero integer. Vehicula gravida sodales habitasse placerat tellus tempus quisque varius commodo senectus montes tristique aenean sit elit curae;. Nisi quis cursus mauris est at nam libero. Quam ultrices eros velit vestibulum. Etiam enim eleifend imperdiet metus hendrerit ridiculus vel taciti augue pede consectetuer diam pretium. Diam fames neque sed. Vitae fames consequat enim venenatis eu in mattis quis vitae parturient etiam dictum mus, convallis. Leo. Magna sagittis. Suspendisse integer scelerisque sagittis tempor facilisi ridiculus augue aptent ultrices placerat facilisis scelerisque torquent taciti, aliquet fermentum. Duis sit Volutpat rutrum potenti feugiat per rhoncus dignissim egestas dolor duis primis lacinia aptent. Taciti venenatis mauris. Ornare metus quis purus, etiam pellentesque magna libero erat hendrerit dictum. Sodales augue risus. Aenean consequat. Pretium torquent turpis ligula, lectus lacinia aliquam interdum sociis varius sem magna arcu parturient lectus taciti dapibus ut viverra cras diam. Commodo posuere sem tellus arcu luctus enim. Ullamcorper hendrerit euismod congue justo interdum. Natoque tristique ullamcorper nascetur nibh hymenaeos suspendisse torquent lectus ullamcorper. Felis fermentum sollicitudin. Inceptos hymenaeos nibh lorem nisl pharetra laoreet mus phasellus Mi dui lectus pharetra lacinia auctor vehicula faucibus praesent sociis, ligula. Aenean. Torquent egestas porttitor morbi interdum nunc mattis augue curabitur varius. Duis cras tortor suscipit vitae tellus posuere ac semper ornare ullamcorper iaculis a montes justo dignissim. Cras posuere cum semper iaculis. Pulvinar tempus. Tortor tortor vitae aliquam donec aliquam Quam. Pede in condimentum lorem primis sed volutpat. Phasellus nisl conubia proin fermentum.'}
+    {body: 'Consequat sit torquent enim suscipit mus ipsum, lectus justo sociis vulputate pulvinar nulla turpis ut nec, dis odio interdum elit. Lorem lacus natoque mauris erat enim rutrum egestas rutrum mauris nam natoque suspendisse quisque purus. Ipsum dolor vivamus, imperdiet arcu sociosqu viverra vel interdum. Dignissim cum potenti mi. Consequat habitant viverra at vitae malesuada interdum. Turpis quam laoreet est conubia in hendrerit ultricies velit facilisi malesuada metus dignissim duis sollicitudin fusce velit dis posuere ullamcorper a duis id, feugiat, curae; iaculis donec, cras tempor diam dapibus nascetur. Fames mattis adipiscing bibendum magna sodales rutrum lobortis parturient sociis purus. Viverra ipsum ornare, taciti in. Mauris libero, facilisi hymenaeos Dis. Nibh nullam curabitur cubilia orci Sagittis diam sit mauris egestas Ac hymenaeos auctor purus. Sollicitudin. At bibendum vel justo parturient etiam tempus torquent ornare rutrum. Parturient nunc posuere nulla eros varius sodales eleifend conubia class molestie ac etiam. Mauris cursus potenti consectetuer a nullam etiam bibendum metus id convallis orci quis malesuada commodo. Convallis. Lacus. Cras. Lacinia urna semper odio eleifend aenean. Mauris viverra ante vel sem malesuada cubilia lacus, placerat facilisis gravida aliquam et hymenaeos tellus penatibus libero quisque sociosqu ridiculus donec eleifend commodo Imperdiet praesent rhoncus vehicula diam fames curae;. Malesuada. Duis posuere ac. Sociis suspendisse magnis nonummy. Nam litora aptent blandit nisi. Pulvinar diam. Blandit amet porttitor nam non urna lacinia eget Sed molestie mauris aliquam ridiculus arcu congue conubia malesuada mattis. Quis nascetur sodales primis laoreet lectus fames suscipit netus. Lacus vehicula dictumst mattis nam non platea orci duis senectus placerat est tortor vestibulum porta sed tortor risus pharetra libero integer. Vehicula gravida sodales habitasse placerat tellus tempus quisque varius commodo senectus montes tristique aenean sit elit curae;.'}
   ];
   res.render('about', { title: 'about', aboutus});
+})
+
+// blog routes
+app.get('/blogs', (req, res) => {
+  Blog.find().sort({ createdAt: -1 })
+    .then((result) => {
+      res.render('index', { title: 'all blogs', blogs: result })
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 })
 
 app.get('/blogs/create', (req, res) => {
